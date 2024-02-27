bd4e9531e34b7bc145a171c0908c32f633050010
rafael <rhirata.s@gmail.com>
2024-02-27 14:41:50 -0300

    Basic API functionality.

---
+++ back-end-maptriz/src/main/java/com/desafio/agenda/model/User.java
@@ -0,0 +1,63 @@
+package com.desafio.agenda.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.IdGeneratorType;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    Long id;
+    String name;
+    String email;
+    String password;
+
+    public User() { };
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ back-end-maptriz/src/main/java/com/desafio/agenda/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.desafio.agenda.repository;
+
+import com.desafio.agenda.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
