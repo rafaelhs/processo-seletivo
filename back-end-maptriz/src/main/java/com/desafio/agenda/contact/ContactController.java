5eb948a39f9432ed0f57ba0813730d85214f16e2
rafael <rhirata.s@gmail.com>
2024-03-01 14:56:16 -0300

    Added get method for a single contact

--- back-end-maptriz/src/main/java/com/desafio/agenda/contact/ContactController.java
+++ back-end-maptriz/src/main/java/com/desafio/agenda/contact/ContactController.java
@@ -18,6 +18,15 @@ public class ContactController {
         this.contactRepository = contactRepository;
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable("id") Long id) {
+        Optional<Contact> contact = contactRepository.findOneById(id);
+        if(contact.isPresent()) {
+            return ResponseEntity.ok().body(contact.get());
+        }
+        return ResponseEntity.badRequest().body(null);
+    }
+
     @PostMapping("/create")
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
         return ResponseEntity.ok().body(contactRepository.save(contact));
