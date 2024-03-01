617e9c3addba951bca4970742778fd8c564e36af
rafael <rhirata.s@gmail.com>
2024-03-01 18:10:24 -0300

    Cleanup

--- front-end-maptriz/src/app/app.component.ts
+++ front-end-maptriz/src/app/app.component.ts
@@ -12,34 +12,12 @@ import { Contact } from 'src/shared/models/contact';
 })
 export class AppComponent implements OnInit {
   title: string = 'agenda';
-  test: string = "ayy lmao";
-  toggle: boolean = true;
-
-  userList! : User[];
-
-  contact: Contact = new Contact(1, "rafael", "hirata", "email@email.com", 
-  4312121212, null, null, null, null, null, null, null, null);
-  contactList: Contact[] = [
-    new Contact(1, "rafael", "hirata", "email@email.com", 
-    4312121212, null, null, null, null, null, null, null, null),
-    new Contact(2, "rafael2", "hirata2", "email2@email.com", 
-    4312121212, null, null, null, null, null, null, null, null),
-    new Contact(3, "rafael3", "hirata3", "email3@email.com", 
-    4312121212, null, null, null, null, null, null, null, null)
-  ]
 
 
-
-  constructor(events: EventService, private userService: UserService) {
-    events.listen('removeUser', (user: User) => {
-      console.log(user.email);
-    })
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((data: any) => {
-        this.userList = data;
-      });
   }
 
   filterApp : any;
