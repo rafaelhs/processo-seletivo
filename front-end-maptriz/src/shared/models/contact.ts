ca73d04c35c9a5aec72ceda421a313e8e29e0e4b
rafael <rhirata.s@gmail.com>
2024-03-01 14:56:31 -0300

    Contact model

---
+++ front-end-maptriz/src/shared/models/contact.ts
@@ -0,0 +1,21 @@
+import { User } from "./user";
+
+export class Contact {
+  constructor(
+      public id: number | null, 
+      public name: string,
+      public lastName: string | null,
+      public email: string,
+      public phone: number | null,
+      public zip: number | null,
+      public address: string | null,
+      public city: string | null,
+      public state: string | null,
+      public document: number | null,
+      public dateOfBirth: string | null,
+      public dateCreated: string | null,
+      public user: User | null
+      ) {
+
+      }
+}
