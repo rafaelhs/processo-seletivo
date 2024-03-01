8e086d8b16805125e7ca5a0671354fe78ac3ee74
rafael <rhirata.s@gmail.com>
2024-03-01 18:10:09 -0300

    Updated contact form

--- front-end-maptriz/src/app/contact-form/contact-form.component.ts
+++ front-end-maptriz/src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ContactService } from '../contact.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Contact } from 'src/shared/models/contact';
 
 @Component({
@@ -12,12 +12,15 @@ export class ContactFormComponent implements OnInit {
   @Input() contact: any = new Contact(null, "", null, "", null, null, null, null, null, null, null, null, null);
   @Output() contactChange = new EventEmitter<any>();
 
-
+  deleteState: boolean = false;
+  //0 add
+  //1 edit
+  //2 read
   
-  constructor(private services: ContactService, private router: ActivatedRoute) { }
+  constructor(private services: ContactService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.router.paramMap.subscribe((params: ParamMap) => {
+    this.route.paramMap.subscribe((params: ParamMap) => {
       let id = params.get('id');
       if(id) {
         this.services.getContact(id).subscribe((contact: any) => {
@@ -31,6 +34,24 @@ export class ContactFormComponent implements OnInit {
   updateContact(contact: any) {
     this.contact = contact;
     this.contactChange.emit(this.contact);
+  }
+
+  handleRemove() {
+    if(this.deleteState) {
+      this.services.removeContact(this.contact.id);
+      this.router.navigate(['contacts']);
+    } else {
+      this.deleteState = !this.deleteState;
+    }
+  }
+
+  handleReturn() {
+    this.router.navigate(['contacts']);
+  }
+
+  handleSave() {
+    this.services.update(this.contact);
+    this.router.navigate(['contacts']);
   }
 
 }
