bedc5b4ec24d78af555abb265023a31703429257
rafael <rhirata.s@gmail.com>
2024-03-02 21:13:04 -0300

    Updated contact default avatar image.

--- front-end-maptriz/src/app/contact-item/contact-item.component.ts
+++ front-end-maptriz/src/app/contact-item/contact-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ContactService } from '../contact.service';
 import { Router } from '@angular/router';
 
 @Component({
